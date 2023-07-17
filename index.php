28b67b5f5ff5acac1a0615f24800c361adb8678b
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-17 14:59:54 +0200

    added snack 2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// DATI SNACK 1
 $partite = [
     [
         'squadra_casa' => 'Juventus',
@@ -31,6 +32,9 @@ $partite = [
         'punti_ospite' => 1
     ]
 ];
+// DATI SNACK 2
+
+// DATI SNACK 3
 
 ?>
 
@@ -40,15 +44,44 @@ $partite = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous' />
     <title>Snacks</title>
 </head>
+<style>
+    h1 {
+        text-align: center;
+        margin-bottom: 50px;
+    }
+
+    input {
+        margin-bottom: 20px;
+    }
+</style>
 
 <body>
-    <ul>
-        <?php foreach ($partite as $partita) : ?>
-            <li><?= $partita['squadra_casa'] ?> - <?= $partita['squadra_ospite'] ?> | <?= $partita['punti_casa'] ?>-<?= $partita['punti_ospite'] ?></li>
-        <?php endforeach ?>
-    </ul>
+
+    <div class="container rounded border border-info mt-5 p-5">
+        <!-- SNACK 1 -->
+        <h1>SNACK 1</h1>
+        <ul>
+            <?php foreach ($partite as $partita) : ?>
+                <li><?= $partita['squadra_casa'] ?> - <?= $partita['squadra_ospite'] ?> | <?= $partita['punti_casa'] ?>-<?= $partita['punti_ospite'] ?></li>
+            <?php endforeach ?>
+        </ul>
+
+        <!-- SNACK 2 -->
+        <hr class="my-5">
+        <h1>SNACK 2</h1>
+
+        <form action="result.php" method="GET">
+            <input class="form-control" type="text" name="name" id="name" placeholder="Nome">
+            <input class="form-control" type="email" name="mail" id="mail" placeholder="E-mail">
+            <input class="form-control" type="number" name="age" id="age" placeholder="Età">
+            <input class="btn btn-primary" type="submit" value="ACCEDI">
+        </form>
+
+        <!-- SNACK 3 -->
+    </div>
 </body>
 
 </html>
---
+++ result.php
@@ -0,0 +1,32 @@
+<?php
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+$message = 'Accesso negato';
+
+if (strlen($name) > 3 && str_contains($mail, '@') && str_contains($mail, '.') && is_numeric($age)) {
+    $message = 'Accesso consentito';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous' />
+    <title>SNAKS: Result</title>
+</head>
+
+<body>
+    <div class="container pt-5 text-center">
+        <h1><?= $message ?></h1>
+        <br>
+        <a class="btn btn-primary" href="index.php">Torna alla homepage</a>
+    </div>
+</body>
+
+</html>
